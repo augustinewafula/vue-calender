c055d19a8ff82c3995f8abe01a6ed44ef9cbefa1
augustinewafula <augustinetreezy@outlook.com>
2020-08-28 11:38:27 +0300

    gh-pages

---
+++ precache-manifest.66017e5cb51ec5dab07a2de6371751b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08d17045ad1b649a238",
+    "url": "/vue-calender/css/chunk-vendors.191812aa.css"
+  },
+  {
+    "revision": "c69b5c818210c2e7d54e2c5abaac7dee",
+    "url": "/vue-calender/index.html"
+  },
+  {
+    "revision": "190d88aa38060d2492b3",
+    "url": "/vue-calender/js/app.2ac95fdf.js"
+  },
+  {
+    "revision": "f08d17045ad1b649a238",
+    "url": "/vue-calender/js/chunk-vendors.9fee30de.js"
+  },
+  {
+    "revision": "df07c10c38c9391c88ec6b8444fa41b6",
+    "url": "/vue-calender/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-calender/robots.txt"
+  }
+]);
